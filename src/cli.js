b9248307501839b4b7c029d6dca16ce41a77d0ab
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-04-19 18:16:45 +0300

    fix: Add missing bad input error message

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 const { clear } = require('./commands/');
 
-module.exports = function cli() {
+module.exports = function cli(args) {
     this.version = 'Version 1.0.0';
 
     const commands = [
@@ -10,10 +10,14 @@ module.exports = function cli() {
             run: clear
         }
     ];
-
+    let userArgs = args.slice(2);
     let usedCommand = commands.filter(function (command) {
-        return command.name === 'clear';
+        return command.name === userArgs[0];
     })[0];
 
+    if (!usedCommand)
+        return console.log(
+            `Can't find command with name ${userArgs[0] ? userArgs[0] : 'null'}`
+        );
     usedCommand.run();
 };
