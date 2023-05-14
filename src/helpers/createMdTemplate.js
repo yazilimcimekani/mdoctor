a4587564a4773ef213075a9c645ea47b35988433
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-05-14 15:46:51 +0300

    feat: Add new md content helpers

---
+++ src/helpers/createMdTemplate.js
@@ -0,0 +1,20 @@
+import codeBlock from './createMdCodeBlock.js';
+
+function createContent(name, shortDescription, clone_url, install_command) {
+    return `# ${name}
+
+${shortDescription}
+
+## Installation
+
+### Clone
+
+${codeBlock('shell', [clone_url])}
+
+### Install
+
+${codeBlock('shell', ['git clone '+ install_command])}
+`;
+}
+
+export default createContent;
