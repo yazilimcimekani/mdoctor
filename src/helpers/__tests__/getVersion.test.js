44dcb5a1c8cf094f86824595fd5c794ded8a5f28
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-05-17 16:04:19 +0300

    test: Update getVersion test with process.cwd()

--- src/helpers/__tests__/getVersion.test.js
+++ src/helpers/__tests__/getVersion.test.js
@@ -1,10 +1,11 @@
+import path from 'path';
 import getVersion from '../getVersion.js';
 import { readFileSync } from 'fs';
 
-const pkg = JSON.parse(readFileSync('./package.json'));
+const pkg = JSON.parse(readFileSync(path.join(process.cwd(), 'package.json')));
 
 describe('getVersion-test', () => {
     test('returns the version number that same as package.json version', () => {
-        expect(getVersion()).toEqual('v' + pkg.version);
+        expect('v' + pkg.version).toEqual(getVersion());
     });
 });
